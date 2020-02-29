4e4d373d61c6385740cb551c16343f49f1f11cd5
der-b <der-b@der-b.com>
2020-02-29 16:53:37 +0100

    Fix(): comments.

--- software/blink/main.c
+++ software/blink/main.c
@@ -9,26 +9,26 @@
 
 int main(void)
 {
-        // Enable the GPIO port A
+        // enable the GPIO port A
 	rcc_periph_clock_enable(RCC_GPIOA);
 
         // set LED pins as floating output with pull up resistor
 	gpio_mode_setup(LED0_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, LED0_PIN);
 	gpio_mode_setup(LED1_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, LED1_PIN);
 
-        // turn LED0 on
+        // turn on LED0
 	gpio_set(LED0_PORT, LED0_PIN);
 
-        // turn LED0 off
+        // turn off LED1
 	gpio_clear(LED1_PORT, LED1_PIN);
 
 	while(1) {
-                // busy waite some time
+                // busy wait some time
 		for (int i = 0; i < 500000; ++i) {
 			__asm__("nop");
 		}
 
-                // chenge the state of both LEDs
+                // change the state of both LEDs
 		gpio_toggle(LED1_PORT, LED0_PIN | LED1_PIN);
 	}
 
